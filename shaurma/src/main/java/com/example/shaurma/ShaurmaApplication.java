d30d57031f0fdf6c7f426953fef7f9d78e17b3e5
Mukhs <azat_market@mail.ru>
2023-10-07 22:59:51 +0300

    add Security

--- shaurma/src/main/java/com/example/shaurma/ShaurmaApplication.java
+++ shaurma/src/main/java/com/example/shaurma/ShaurmaApplication.java
@@ -1,20 +1,30 @@
 package com.example.shaurma;
 
 import com.example.shaurma.models.Ingredient;
+import com.example.shaurma.models.User;
 import com.example.shaurma.repositories.IngredientRepository;
+import com.example.shaurma.repositories.UserRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class ShaurmaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShaurmaApplication.class, args);
-
-
 	}
 
 	@Bean
@@ -30,6 +40,16 @@ public class ShaurmaApplication {
 			repo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
 			repo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
 			repo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+		};
+	}
+
+
+	@Bean
+	public SecurityConfig.UserDetailsService userDetailsService(UserRepository userRepo) {
+		return username -> {
+			User user = userRepo.findByUsername(username);
+			if (user != null) return user;
+			throw new UsernameNotFoundException("User ‘" + username + "’ not found");
 		};
 	}
 
